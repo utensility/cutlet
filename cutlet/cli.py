fea51a13ad35ff6854ac8d2ecc950e82740bc802
utensility <>
2022-11-04 22:26:31 +0200

    Prevent non-existant SIGPIPE error

--- cutlet/cli.py
+++ cutlet/cli.py
@@ -2,11 +2,16 @@ from cutlet import Cutlet
 import fileinput
 import sys
 
-# Don't print an error on SIGPIPE
-from signal import signal, SIGPIPE, SIG_DFL
 
 def main():
-    signal(SIGPIPE, SIG_DFL) 
+    # Windows doesn't support SIGPIPE.
+    try:
+        # Don't print an error on SIGPIPE
+        from signal import signal, SIGPIPE, SIG_DFL
+        signal(SIGPIPE, SIG_DFL)
+    except ImportError:
+        pass
+    
     system = sys.argv[1] if len(sys.argv) > 1 else 'hepburn'
 
     katsu = Cutlet(system)
